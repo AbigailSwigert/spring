46ba32a0a8a59e74851c63432af8c319104ba168
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-28 17:58:39 -0500

    created new spring boot project, added some packages, and a User class

---
+++ apiExercises/src/main/java/com/tekcamp/apiExercises/model/User.java
@@ -0,0 +1,107 @@
+package com.tekcamp.apiExercises.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String encryptedPassword;
+	private String emailVerificationToken;
+	private Boolean emailVerificationStatus;
+
+	public User() {
+	}
+
+	public User(String userId, String firstName, String lastName, String email, String password,
+				String encryptedPassword, String emailVerificationToken, Boolean emailVerificationStatus) {
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.encryptedPassword = encryptedPassword;
+		this.emailVerificationToken = emailVerificationToken;
+		this.emailVerificationStatus = emailVerificationStatus;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+
+	public String getEmailVerificationToken() {
+		return emailVerificationToken;
+	}
+
+	public void setEmailVerificationToken(String emailVerificationToken) {
+		this.emailVerificationToken = emailVerificationToken;
+	}
+
+	public Boolean getEmailVerificationStatus() {
+		return emailVerificationStatus;
+	}
+
+	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+		this.emailVerificationStatus = emailVerificationStatus;
+	}
+}
---
+++ apiExercises/src/test/java/com/tekcamp/apiExercises/ApiExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekcamp.apiExercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
