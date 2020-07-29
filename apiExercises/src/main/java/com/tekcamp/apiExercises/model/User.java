2718e00fbff122d8c64652ba0edff0cfb9c992c2
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-29 09:09:55 -0500

    added UserRequest

--- apiExercises/src/main/java/com/tekcamp/apiExercises/model/User.java
+++ apiExercises/src/main/java/com/tekcamp/apiExercises/model/User.java
@@ -21,9 +21,6 @@ public class User {
 	@Column(nullable = false, unique = true)
 	private String email;
 
-	@Column(nullable = false)
-	private String password;
-
 	private String encryptedPassword;
 	private String emailVerificationToken;
 	private Boolean emailVerificationStatus;
@@ -31,13 +28,12 @@ public class User {
 	public User() {
 	}
 
-	public User(String userId, String firstName, String lastName, String email, String password,
+	public User(String userId, String firstName, String lastName, String email,
 				String encryptedPassword, String emailVerificationToken, Boolean emailVerificationStatus) {
 		this.userId = userId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.password = password;
 		this.encryptedPassword = encryptedPassword;
 		this.emailVerificationToken = emailVerificationToken;
 		this.emailVerificationStatus = emailVerificationStatus;
@@ -77,14 +73,6 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getEncryptedPassword() {
