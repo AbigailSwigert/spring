9df41f1e3cfcf378938bebfa0350d1bb1d9584b1
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-30 12:36:08 -0500

    added more error handling

--- apiExercises/src/main/java/com/tekcamp/apiExercises/model/response/ErrorMessages.java
+++ apiExercises/src/main/java/com/tekcamp/apiExercises/model/response/ErrorMessages.java
@@ -2,7 +2,11 @@ package com.tekcamp.apiExercises.model.response;
 
 public enum ErrorMessages {
 
-    EMAIL_ALREADY_IN_USE("The email you entered is already in use");
+    EMAIL_ALREADY_IN_USE ("The email you entered is already in use"),
+    REQUIRED_FIELD_MISSING ("Missing one or more required fields. Required fields include: First Name, Last Name, Email, and Password"),
+    EMPTY_USER_LIST("There are no users in the database"),
+    USERID_DOES_NOT_EXIST("There is no user in the database with that userId"),
+    USER_EMAIL_DOES_NOT_EXIST("There is no user in the database with that email");
 
     private String errorMessage;
 
--- apiExercises/src/main/java/com/tekcamp/apiExercises/service/implementations/UserServiceImplementation.java
+++ apiExercises/src/main/java/com/tekcamp/apiExercises/service/implementations/UserServiceImplementation.java
@@ -30,11 +30,13 @@ public class UserServiceImplementation implements UserService {
     public List<User> getUsers() {
         List<User> returnValue;
         returnValue = (List<User>) userRepository.findAll();
+        if (returnValue.size() < 1) throw new UserServiceException(ErrorMessages.EMPTY_USER_LIST.getErrorMessage());
         return returnValue;
     }
 
     @Override
     public UserDto createUser(UserDto userDto) {
+        if (userDto.getFirstName() == null | userDto.getLastName() == null | userDto.getEmail() == null | userDto.getPassword() == null) throw new UserServiceException(ErrorMessages.REQUIRED_FIELD_MISSING.getErrorMessage());
         if (userRepository.findByEmail(userDto.getEmail()) != null) throw new UserServiceException(ErrorMessages.EMAIL_ALREADY_IN_USE.getErrorMessage());
         User newUser = new User();
         BeanUtils.copyProperties(userDto, newUser);
@@ -51,12 +53,14 @@ public class UserServiceImplementation implements UserService {
     @Override
     public User getUserByUserId(String userId) {
         User returnValue = userRepository.findByUserId(userId);
+        if (returnValue == null) throw new UserServiceException(ErrorMessages.USERID_DOES_NOT_EXIST.getErrorMessage());
         return returnValue;
     }
 
     @Override
     public User getUserByEmail(String email) {
         User returnValue = userRepository.findByEmail(email);
+        if (returnValue == null) throw new UserServiceException(ErrorMessages.USER_EMAIL_DOES_NOT_EXIST.getErrorMessage());
         return returnValue;
     }
 }
